f0ec1e1d7519424fad6953cbbed9770b33eaa57e
Gianluca Violoni <violoni.gianluca@gmail.com>
2024-10-03 00:38:28 +0200

    list command

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,45 @@
-use std::env;
+use std::{env, io, fs};
 use std::path::Path;
 use std::fs::File;
-use std::fs;
-/*
-fn list(file: &File) -> Result<(),> {
+use std::io::{BufRead, BufReader};
 
+fn format_line(line: String) -> Option<String> {
+    let mut formatted = String::new();
+    let mut chars = line.chars();
+
+    match chars.next() {
+        Some('x') => formatted.push_str("[x] "),
+        Some('o') => formatted.push_str("[ ] "),
+        _ => return None,
+    };
+
+    if chars.next().is_none() {
+        return None;
+    }
+
+    formatted.extend(chars);
+
+    Some(formatted)
+}
+
+fn list(file: &File) -> Result<(), io::Error> {
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        match line {
+            Ok(l) => {
+                match format_line(l) {
+                    Some(str) => println!("{}", str),
+                    None => return Err(io::Error::new(io::ErrorKind::InvalidData, "malformed line")),
+                };
+            },
+            Err(r) => return Err(r),
+        }
+    }
 
     Ok(())
 }
-*/
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -45,14 +76,17 @@ fn main() {
         Err(r) => panic!("{}", r),
     };
 
-    file;
-
-    match cmd {
-        "list" => {},
-        "mark" => {},
-        "add" => {},
-        "remove" => {},
-        "clean" => {},
+    let result = match cmd {
+        "list" => list(&file),
+        "mark" => Ok(()),
+        "add" => Ok(()),
+        "remove" => Ok(()),
+        "clean" => Ok(()),
         _ => panic!("invalid args"),
+    };
+
+    match result {
+        Ok(_) => (),
+        Err(r) => panic!("{}", r),
     }
 }
