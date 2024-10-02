36f1f041a729780aa3cced115bc71fe5ddf993b1
Gianluca Violoni <violoni.gianluca@gmail.com>
2024-10-03 01:51:04 +0200

    🦑 add command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{env, io, fs};
 use std::path::Path;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 
 fn format_line(line: String) -> Option<String> {
     let mut formatted = String::new();
@@ -22,20 +22,72 @@ fn format_line(line: String) -> Option<String> {
     Some(formatted)
 }
 
-fn list(file: &File) -> Result<(), io::Error> {
+fn list(file: File) -> Result<(), io::Error> {
     let reader = BufReader::new(file);
 
-    for line in reader.lines() {
+    for (i, line) in reader.lines().enumerate() {
         match line {
             Ok(l) => {
                 match format_line(l) {
-                    Some(str) => println!("{}", str),
+                    Some(str) => println!("{:0>2} {}", i+1, str),
                     None => return Err(io::Error::new(io::ErrorKind::InvalidData, "malformed line")),
                 };
             },
             Err(r) => return Err(r),
         }
     }
+
+    Ok(())
+}
+
+fn add(file: File, file_path: &Path, tmp_file_path: &Path,  args: Vec<String>) -> Result<(), io::Error> {
+    let mut tmp_file = File::create(tmp_file_path)?;
+    let reader = BufReader::new(&file);
+
+    let sub_task = match args.get(2) {
+        Some(x) => x == "-s",
+        None => return Err(io::Error::new(io::ErrorKind::InvalidData, "invalid args")),
+    };
+
+    let mut sub_task_parent: usize = 0;
+    if sub_task {
+        let parent_string = match args.get(3) {
+            Some(x) => x,
+            None => return Err(io::Error::new(io::ErrorKind::InvalidData, "invalid args")),
+        };
+
+        sub_task_parent = match parent_string.parse::<usize>() {
+            Ok(num) => num,
+            Err(_) => return Err(io::Error::new(io::ErrorKind::InvalidData, "invalid number format")),
+        }
+    }
+
+    for (i, line) in reader.lines().enumerate() {
+        match line {
+            Ok(l) => {
+                writeln!(tmp_file, "{}", l)?;
+                if i+1 == sub_task_parent {
+                    let chunks: Vec<&str> = l.split(' ').collect();
+                    let line_depth = match chunks.get(1) {
+                        Some(&x) => x.chars().count(),
+                        None => return Err(io::Error::new(io::ErrorKind::InvalidData, "invalid number format")),
+                    };
+
+                    writeln!(tmp_file, "o {} {}", "─".repeat(line_depth+1), args[4..].join(" "))?;
+                }
+            },
+            Err(r) => return Err(r),
+        }
+    }
+
+    if !sub_task {
+        writeln!(tmp_file, "o ─ {}", args[2..].join(" "))?;
+    }
+
+    drop(file);
+    drop(tmp_file);
+
+    fs::rename(tmp_file_path, file_path)?;
 
     Ok(())
 }
@@ -65,6 +117,9 @@ fn main() {
         panic!("error on creating dir: {}", parent_path.to_str().unwrap());
     }
 
+    let tmp_path = parent_path.join("tmp");
+    let tmp_path = tmp_path.as_path();
+
     let file = if path.exists() {
         File::open(path)
     } else {
@@ -77,13 +132,14 @@ fn main() {
     };
 
     let result = match cmd {
-        "list" => list(&file),
+        "list" => list(file),
         "mark" => Ok(()),
-        "add" => Ok(()),
+        "add" => add(file, path, tmp_path, args),
         "remove" => Ok(()),
         "clean" => Ok(()),
         _ => panic!("invalid args"),
     };
+
 
     match result {
         Ok(_) => (),
