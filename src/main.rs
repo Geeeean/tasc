2b68953a32d056f8d5f2638e15728a8884a81be6
Gianluca Violoni <violoni.gianluca@gmail.com>
2024-10-03 14:36:01 +0200

    🐭 better print on list command

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,42 @@ use std::path::Path;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
 
-fn format_line(line: String) -> Option<String> {
+fn format_line(line: &(String, usize), prev_depth: usize, next_depth: usize) -> Option<String> {
     let mut formatted = String::new();
-    let mut chars = line.chars();
 
-    match chars.next() {
-        Some('x') => formatted.push_str("[x] "),
-        Some('o') => formatted.push_str("[ ] "),
-        _ => return None,
+    let line_depth = line.1;
+    let line = &line.0;
+
+    let chunks: Vec<&str> = line.split(' ').collect();
+
+    let marked = match chunks.get(0) {
+        Some(&x) => x == "x",
+        None => return None,
     };
 
-    if chars.next().is_none() {
-        return None;
+    if marked {
+        formatted.push_str("[x]");
+    } else {
+        formatted.push_str("[ ]");
     }
 
-    formatted.extend(chars);
+    formatted.push(' ');
+
+    if line_depth > 1 {
+        formatted.push_str(&"│  ".repeat(line_depth-1)) ;
+
+        if line_depth == next_depth {
+            formatted.push_str("├─");
+        } else {
+            formatted.push_str("└─");
+        }
+    } else {
+        formatted.push_str("├─");
+    }
+
+    formatted.push(' ');
+
+    formatted.push_str(&chunks[2..].join(" "));
 
     Some(formatted)
 }
@@ -25,16 +46,54 @@ fn format_line(line: String) -> Option<String> {
 fn list(file: File) -> Result<(), io::Error> {
     let reader = BufReader::new(file);
 
-    for (i, line) in reader.lines().enumerate() {
-        match line {
-            Ok(l) => {
-                match format_line(l) {
-                    Some(str) => println!("{:0>2} {}", i+1, str),
-                    None => return Err(io::Error::new(io::ErrorKind::InvalidData, "malformed line")),
-                };
-            },
-            Err(r) => return Err(r),
-        }
+    //a bit of shadowing here 🙈
+    let lines: io::Result<Vec<String>> = reader.lines().collect();
+
+    let lines = match lines {
+        Ok(res) => res,
+        Err(e) => return Err(e),
+    };
+
+    let lines: io::Result<Vec<(String, usize)>> = lines.into_iter().map(|line| {
+        let chunks: Vec<&str> = line.split(' ').collect();
+
+        let line_depth = match chunks.get(1) {
+            Some(&x) => x.parse::<usize>(),
+            None => return Err(io::Error::new(io::ErrorKind::InvalidData, "invalid number format")),
+        };
+
+        let line_depth = match line_depth {
+            Ok(num) => num,
+            Err(_) => return Err(io::Error::new(io::ErrorKind::InvalidData, "invalid number format")),
+        };
+
+        Ok((line, line_depth))
+    }).collect();
+
+    let lines = match lines {
+        Ok(res) => res,
+        Err(r) => return Err(r),
+    };
+
+    for (i, line) in lines.iter().enumerate() {
+        let prev_depth = if i != 0 {
+            match lines.get(i-1) {
+                Some(x) => x.1,
+                None => 0,
+            }
+        } else {
+            0
+        };
+
+        let next_depth = match lines.get(i+1) {
+            Some(x) => x.1,
+            None => 0,
+        };
+
+        match format_line(line, prev_depth, next_depth) {
+            Some(str) => println!("{:0>2} {}", i+1, str),
+            None => return Err(io::Error::new(io::ErrorKind::InvalidData, "malformed line")),
+        };
     }
 
     Ok(())
@@ -97,11 +156,16 @@ fn add(file: File, file_path: &Path, tmp_file_path: &Path,  args: Vec<String>) -
                 if i+1 == sub_task_parent {
                     let chunks: Vec<&str> = l.split(' ').collect();
                     let line_depth = match chunks.get(1) {
-                        Some(&x) => x.chars().count(),
+                        Some(&x) => x.parse::<usize>(),
                         None => return Err(io::Error::new(io::ErrorKind::InvalidData, "invalid number format")),
                     };
 
-                    writeln!(tmp_file, "o {} {}", "─".repeat(line_depth+1), args[4..].join(" "))?;
+                    let line_depth = match line_depth {
+                        Ok(num) => num,
+                        Err(_) => return Err(io::Error::new(io::ErrorKind::InvalidData, "invalid number format")),
+                    };
+
+                    writeln!(tmp_file, "o {} {}", line_depth+1, args[4..].join(" "))?;
                 }
             },
             Err(r) => return Err(r),
